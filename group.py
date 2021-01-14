8959f2608efd6bab479cb0acd2571c90265dcc13
Omar Mir <omarmir@gmail.com>
2021-01-14 12:49:36 -0500

    Successfully parsed id3

--- group.py
+++ group.py
@@ -1,16 +1,23 @@
 class RateGroup:
-    def __init__(self, group, level, grpLvl, rateAgreements):
+    def __init__(self, grpLvl, group, level, rateAgreements):
         self.grpLvl = grpLvl
         self.group = group
         self.level = level
         self.rateAgreements = rateAgreements
+    def to_dict(self):
+        return {"grpLvl": self.grpLvl, "group": self.group, "level": self.level, "rateAgreements" : [rateAgreenent.to_dict() for rateAgreenent in self.rateAgreements]}
 
 class RateAgreement:
     def __init__(self, effectiveDate, rateStepsList):
         self.effectiveDate = effectiveDate
         self.rateStepsList = rateStepsList
+    def to_dict(self):
+        return {"effectiveDate": self.effectiveDate, "rateStepsList": [rateStep.to_dict() for rateStep in self.rateStepsList]}
+
 
 class RateSteps:
     def __init__(self, step, amount):
         self.step = step
         self.amount = amount
+    def to_dict(self):
+        return {"step": self.step, "amount": self.amount}
